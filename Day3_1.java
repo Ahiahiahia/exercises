40e43afdc7a62fa5a76d2e8cd45343b147a69391
Ahiahiahia <1030036999@qq.com>
2019-03-10 23:10:12 +0800

    求两个数的公倍数

---
+++ Day3_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by hunter on 2019/3/10
+ */
+public class Day3_1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        if(in.hasNextLine()){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            System.out.println(findSum(n, m));
+        }
+    }
+    public static int findSum(int a, int b){
+        for(int i=1; ; i++){
+            int temp = a*i;
+            if(temp%b == 0){
+                return temp;
+            }
+        }
+    }
+}
