8128113367896e6a0e195d6fec5a1d69780dadd5
Ahiahiahia <1030036999@qq.com>
2019-03-17 23:22:59 +0800

    求两个数组的交集

--- Test8.java
+++ Test8.java
@@ -1,33 +1,47 @@
-import java.util.Scanner;
-
 /**
- * 小易去商店买苹果，只有6个一袋和8个一袋的包装。
- * 小易恰好只购买n个苹果，购买尽量少的袋数方便携带。
- * 如果不能恰好购买n个，将不会购买。
- * 输出一个整数表示恰好购买的袋数，若不能买恰好n个苹果，就输出-1.
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 示例 1:
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 示例 2:
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
  */
-
-public class Test8 {
+class Test8 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        int count = n / 8;
-        int tmp = n % 8;  //计算每次分配之后剩余的苹果数
-        if (tmp == 0){
-            System.out.println(count);
-            return;
-        }else {
-            while (count != 0){
-                count--;
-                tmp = tmp + 8;
-                if (tmp % 6 == 0){
-                    count = count + (tmp/6);
-                    System.out.println(count);
-                    return;
-                }
-            }
-            System.out.println(-1);
+        int[] nums1 = new int[]{1, 2, 2, 1};
+        int[] nums2 = new int[]{2, 2};
+        int[] result = intersection(nums1, nums2);
+        for(int i : result){
+            System.out.println(i + "、");
         }
     }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new TreeSet<>();
+        if(nums1.length == 0 || nums2.length == 0){
+            return new int[0];
+        }
+        Arrays.sort(nums1);
+        for(int i=0; i<nums1.length-1; i++){
+            if(nums1[i] == nums1[i+1]){
+                continue;
+            }
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    set.add(nums2[j]);
+                }
+            }
+        }
+        for(int j=0; j<nums2.length; j++){
+            if(nums1[nums1.length-1] == nums2[j]){
+                set.add(nums2[j]);
+            }
+        }
+        int[] tmp = new int[set.size()];
+        int i=0;
+        for(int temp : set){
+            tmp[i++] = temp;
+        }
+        return tmp;
+    }
 }
-
